37b4ce6e9fc8a7b6c3aed5545be3e7d392a15c22
yinoue93 <yinoue93@stanford.edu>
2017-03-03 15:17:02 -0800

    moving to azure for sec

--- utils_preprocess.py
+++ utils_preprocess.py
@@ -5,29 +5,6 @@ import re
 
 from multiprocessing import Pool
 
-def drummer():
-	iterf = 0
-	for kk in os.listdir('video_games'):
-		try:
-			if iterf%100 == 0:
-				print iterf
-			iterf += 1
-			midi_data = pretty_midi.PrettyMIDI('video_games/'+kk)
-			roll = midi_data.get_piano_roll()
-
-			count = 0
-			for i in midi_data.instruments:
-				if i.is_drum:
-					count += 1
-
-			if count>1:
-				ccount += 1
-				#print kk
-				#print '--'+str(count)
-		except:
-			print 'failed: '+kk
-	print ccount
-
 def eraseUnreadable(folderN):
 	iterf = 0
 	for fileN in os.listdir(folderN):
@@ -54,24 +31,37 @@ def extractABCtxtWorker(dataPack):
 	header = True
 	headerDict = {}
 	headerTup = ('T', 'R', 'M', 'L', 'K', 'Q')
+	headerDefault = {'T':'none', 'R':'none', 'M':'none', 'L':'none', 'K':'C', 'Q':'100'}
+	print filename
 	with open(filename,'r') as infile:
+		fileStr = infile.read().replace(' ','')
+		if fileStr.replace('\n','')=='':
+			return
+
 		with open(outputname,'w') as outfile:
-			for line in infile:
-				line = line.replace(' ','')
-				if line=='\n':
+			for line in fileStr.split('\n'):
+				line = line.strip()
+				# skip empty lines
+				if line=='':
 					continue
+
 				if header:
 					headerStr = re.match('[a-zA-Z]:',line)
+
 					if headerStr is None:
 						header = False
 						for head in headerTup:
 							if head in headerDict:
-								outfile.write(headerDict[head])
-						outfile.write(line.replace('\n', ''))
+								outfile.write(headerDict[head]+'\n')
+							else:
+								outfile.write(headerDefault[head]+'\n')
+						outfile.write(line)
+
 					elif headerStr.group()[0] in headerTup:
 						headerDict[headerStr.group()[0]] = line
+
 				else:
-					outfile.write(line.replace('\n', ''))
+					outfile.write(line)
 
 			outfile.write('\n')
 
@@ -82,12 +72,15 @@ def extractABCtxt(folderName):
 		os.makedirs(outputFolder)
 
 	p = Pool(8)
-	mapList = [(os.path.join(folderName,fname),os.path.join(outputFolder,fname)) 
-										for fname in os.listdir(folderName)]
+	filenames = [re.sub(r'[^\x00-\x7f]',r'',fname) for fname in os.listdir(folderName)]
+	mapList = [(os.path.join(folderName,fname),os.path.join(outputFolder,fname2)) 
+										for fname,fname2 in zip(os.listdir(folderName),filenames)]
 
 	p.map(extractABCtxtWorker, mapList)
 
+
 if __name__ == "__main__":
-	extractABCtxt('/data/the_session')
+	#extractABCtxt('the_session')
+	extractABCtxtWorker(('the_session/A French Dance jig_0.abc','tmp.abc'))
 	# eraseUnreadable('video_games')
 	#plotMIDI('video_games/dw2.mid')
